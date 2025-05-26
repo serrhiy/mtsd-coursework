a533db74484c5cc02c42483a59f7c8c793dd4b91
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-27 00:08:26 +0300

    Move to WebSocket

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"net"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 const host = "0.0.0.0"
 const port = "8080"
+
 var address = net.JoinHostPort(host, port)
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    1 << 10,
+	WriteBufferSize:   1 << 10,
+	HandshakeTimeout:  time.Second * 3,
+	EnableCompression: true,
+	CheckOrigin:       func(*http.Request) bool { return true },
+}
+
 func onRequest(writer http.ResponseWriter, request *http.Request) {
-	io.WriteString(writer, "<h1>Hello world!</h1>")
+	connection, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		fmt.Println(err, request.Header["Origin"])
+		return
+	}
+	defer connection.Close()
+	connection.WriteMessage(websocket.TextMessage, []byte("Hello world!"))
 }
 
 func main() {
