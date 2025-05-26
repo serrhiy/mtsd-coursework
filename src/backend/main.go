5565692b5212938a5e7bf9dba6ad16581f08aa64
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-26 23:39:52 +0300

    Move to http

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net"
+	"net/http"
+)
+
+const host = "0.0.0.0"
+const port = "8080"
+var address = net.JoinHostPort(host, port)
+
+func onRequest(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "<h1>Hello world!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", onRequest)
+	http.ListenAndServe(address, nil)
 }
