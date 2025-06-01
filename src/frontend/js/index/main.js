b93e08d0fd6e8b1aac967d282cf0c440e647b549
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-06-01 19:46:54 +0300

    Implement basic frontend

---
+++ src/frontend/js/index/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import WebSocketTransport from "../WebSocketTransport.js";
+import structure from "./structure.js";
+import scaffold from "../scaffold.js";
+
+const saveUsernameButton = document.getElementById('saveUsernameButton');
+const saveChatButton = document.getElementById('createRoomButton');
+const usernameInput = document.getElementById('usernameInput');
+const roomNameInput = document.getElementById('roomNameInput');
+
+const onUsernameButton = (api) => {
+  const source = usernameInput.value.trim();
+  if (source.length === 0) return;
+};
+
+const onChatButton = (api) => {
+  const source = roomNameInput.value.trim();
+  if (source.length === 0) return;
+};
+
+const main = async () => {
+  const websocket = await new WebSocketTransport('ws://127.0.0.1:8080');
+  const api = scaffold(structure, websocket);
+  console.log(await api.users.exists());
+  saveUsernameButton.addEventListener('click', onUsernameButton.bind(null, api));
+  saveChatButton.addEventListener('click', onChatButton.bind(null, api));
+};
+
+main();
---
+++ src/frontend/js/index/structure.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export default {
+  users: ['create', 'update', 'exists'],
+  rooms: ['create', 'get']
+};
