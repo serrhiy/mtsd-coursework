1bede4235b3f8c7d67de0cd95b2bc2288d964d7e
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-06-03 22:51:42 +0300

    Add user creations in frontend

--- src/frontend/js/index/main.js
+++ src/frontend/js/index/main.js
@@ -3,6 +3,7 @@
 import WebSocketTransport from "../WebSocketTransport.js";
 import structure from "./structure.js";
 import scaffold from "../scaffold.js";
+import { getRandomUsername } from "./utils.js";
 
 const saveUsernameButton = document.getElementById('saveUsernameButton');
 const saveChatButton = document.getElementById('createRoomButton');
@@ -19,10 +20,36 @@ const onChatButton = (api) => {
   if (source.length === 0) return;
 };
 
+const setupUsername = async () => {
+  if (localStorage.getItem('username') === null) {
+    const username = await getRandomUsername();
+    localStorage.setItem('username', username);
+  }
+  const username = localStorage.getItem('username');
+  usernameInput.value = username;
+  return username;
+}
+
+const setupToken = async (api) => {
+  if (localStorage.getItem('token') === null) {
+    if (localStorage.getItem('username') === null) setupUsername();
+    const username = localStorage.getItem('username');
+    const token = await api.users.create(username);
+    localStorage.setItem('token', token);
+    return token;
+  }
+  const token = localStorage.getItem('token');
+  const exists = await api.users.exists(token);
+  if (exists) return token;
+  localStorage.removeItem('token');
+  return setupToken();
+};
+
 const main = async () => {
   const websocket = await new WebSocketTransport('ws://127.0.0.1:8080');
   const api = scaffold(structure, websocket);
-  console.log(await api.users.exists("asd"));
+  await setupUsername();
+  await setupToken(api);
   saveUsernameButton.addEventListener('click', onUsernameButton.bind(null, api));
   saveChatButton.addEventListener('click', onChatButton.bind(null, api));
 };
---
+++ src/frontend/js/index/utils.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const API = 'https://randomuser.me/api?results=1&nat=ua&inc=name';
+
+export const getRandomUsername = async () => {
+  const signal = AbortSignal.timeout(500);
+  const response = await fetch(API, { signal });
+  if (!response.ok) throw new Error('Invalid request');
+  const json = await response.json();
+  if (json.error) throw new Error(json.error);
+  const { first, last } = json.results[0].name;
+  return first + '-' + last;
+};
